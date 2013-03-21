f7b3cf990285c484777ae56ca30b805b832cd97b
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2013-03-21 16:29:17 +0200

    [os] path.isSymLink()
    also more unit tests
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- tests/test_os.js
+++ tests/test_os.js
@@ -1,5 +1,6 @@
 var test = require('test');
 var os = require("os.js");
+var ps = require('subprocess');
 
 test.execute({
     basic : function() {
@@ -10,6 +11,7 @@ test.execute({
     },
     path : function() {
         test.equal(os.path(), '');
+        test.equal(os.path('', '/'), '/');
         test.equal(os.path('/'), '/');
         test.equal(os.path('/', 'usr'), '/usr');
         test.equal(os.path('/usr', 'bin'), '/usr/bin');
@@ -23,6 +25,15 @@ test.execute({
         test.deepEqual(os.path.split('/usr///bin/'), ['/', 'usr', 'bin']);
         test.deepEqual(os.path.split('usr///bin'), ['usr', 'bin']);
     },
+    file_io : function() {
+        var data = 'a\nb'
+          , fname = '/tmp/cutes_lib_test_aa';
+        os.write_file(fname, data);
+        test.equal(os.path.exists(fname), true);
+        test.equal(os.path.isfile(fname), true);
+        var read = os.read_file(fname);
+        test.equal(read, data);
+    },
     path_checks : function() {
         test.equal(os.path.exists('non_existing_file'), false);
         test.equal(os.path.exists(os.home()), true);
@@ -34,14 +45,12 @@ test.execute({
         test.equal(os.path.isfile('non_existing_file'), false);
 
         test.ok(os.path.isSame('/tmp', '/tmp/../tmp', "absolute and relative"));
-    },
-    file_io : function() {
-        var data = 'a\nb'
-          , fname = '/tmp/cutes_lib_test_aa';
-        os.write_file(fname, data);
-        test.equal(os.path.exists(fname), true);
-        test.equal(os.path.isfile(fname), true);
-        var read = os.read_file(fname);
-        test.equal(read, data);
+
+        ps.system('touch', ['/tmp/cutes_lib_test_file1']);
+        ps.system('ln' , ['-s', '/tmp/cutes_lib_test_file1'
+                          , '/tmp/cutes_lib_test_link1']);
+        test.ok(os.path.isSymLink('/tmp/cutes_lib_test_link1'), "Should be link");
+
+    }
     }
 });
