a9d3a8ba36c743d705b7df9092e0229427ceb227
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-02-05 12:35:35 +0200

    [test] adding subprocess tests
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

---
+++ tests/test_subprocess.js
@@ -0,0 +1,50 @@
+var util = require('util');
+var test = require('test');
+var os = require('os');
+var api = require('subprocess');
+
+var fixture = test.fixture();
+
+fixture.addTest('ps_call_rc', function() {
+    var test_cmd = "./subprocess_test_cmd.sh";
+    var ps, res;
+    ps = api.process();
+    res = ps.call_rc(test_cmd);
+    test.equal(res, 0);
+});
+
+fixture.addTest('ps_check_output', function() {
+    var test_cmd = "./subprocess_test_cmd.sh";
+    var ps, res;
+    ps = api.process();
+    res = ps.check_output(test_cmd);
+    test.equal(res.toString(), "1\n2\n");
+});
+
+fixture.addTest('ps_cwd', function() {
+    var ps, res;
+    var dir_name = os.mktemp({dir: true});
+    test.ok(os.path.isDir(dir_name), "temporary dir?");
+    var f1 = os.path(dir_name, "f1");
+    os.write_file(f1);
+    test.ok(os.path.isFile(f1), "f1 in temporary dir?");
+
+    ps = api.process({cwd: dir_name});
+    res = ps.check_output("ls", [dir_name]);
+    test.equal(res.toString(), "f1\n");
+
+    os.rmtree(dir_name);
+    test.ok(!os.path.isDir(dir_name), "temporary dir is deleted?");
+
+    var dir2 = os.mktemp({dir: true});
+    test.ok(os.path.isDir(dir2), "temporary dir2?");
+    f2 = os.path(dir2, "f2");
+    os.write_file(f2);
+    test.ok(os.path.isFile(f2), "f2 in temporary dir?");
+
+    ps = api.process({cwd: dir2});
+    res = ps.check_output("ls", [dir2]);
+    test.equal(res.toString(), "f2\n");
+});
+
+fixture.execute();
