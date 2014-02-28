712edfa48ba955ebc13ca5f9f189eca6eb4856c1
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-02-28 19:54:39 +0200

    [functional] move mapObject from util, arrayToSet, separate doc module
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tests/test_functional.js
+++ tests/test_functional.js
@@ -27,4 +27,30 @@ fixture.addTest('functionStack', function() {
     test.equal(s, '23');
 });
 
+fixture.addTest('arrayToSet', function() {
+    var res;
+    res = api.arrayToSet([]);
+    test.deepEqual(res, {});
+    res = api.arrayToSet(['a', 'b']);
+    test.deepEqual(res, {a: true, b: true});
+    res = api.arrayToSet(['a', 'b', 'a']);
+    test.deepEqual(res, {a: true, b: true});
+});
+
+fixture.addTest('mapObject', function() {
+    var res;
+    var toStr = function(value, key) {
+        return String(key) + "," + value;
+    };
+    res = api.mapObject(toStr, {});
+    test.deepEqual(res, []);
+
+    res = api.mapObject(toStr, {x:1});
+    test.deepEqual(res, ["x,1"]);
+
+    res = api.mapObject(toStr, {x:1, y:3});
+    res.sort();
+    test.deepEqual(res, ["x,1", "y,3"]);
+});
+
 fixture.execute();
