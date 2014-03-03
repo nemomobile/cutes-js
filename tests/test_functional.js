da093b4b9a63dcf15c26b82f22509b8ba6fd35fd
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-03-03 16:21:15 +0200

    [functional] visit() logic is changed :/
    Now it also traverse the root node (name === null) and returns the
    result of visitor execution on the root node.
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tests/test_functional.js
+++ tests/test_functional.js
@@ -2,21 +2,9 @@ var config = require('config');
 config.document = true;
 var test = require('test');
 var api = require("functional");
+var util = require("util");
 
 var fixture = test.fixture();
-
-fixture.addTest('visit', function() {
-    var data = [];
-    api.visit(function(ctx, k, v) {
-        var kv;
-        kv = ((typeof k === 'string') ? k : "")
-            + ((typeof v === 'string') ? v : "");
-        var res = ctx ? ctx + kv : kv;
-        data.push(res);
-        return res;
-    }, { a : { b : 'c', d : 'e', f : ['g', 'h'] } });
-    test.deepEqual(data, ["a","abc","ade","af","af0g","af1h"]);
-});
 
 fixture.addTest('functionStack', function() {
     var s = "";
@@ -52,5 +40,42 @@ fixture.addTest('mapObject', function() {
     res.sort();
     test.deepEqual(res, ["x,1", "y,3"]);
 });
+
+fixture.addTest('visit', function() {
+    var data = [];
+    var src1 = { a : { b : 'c', d : 'e', f : ['g', 'h'] } };
+    var res = api.visit(function(ctx, k, v) {
+        if (k === null)
+            return "";
+        var kv;
+        kv = ((typeof k === 'string') ? k : "")
+            + ((typeof v === 'string') ? v : "");
+        var res = ctx + kv;
+        data.push(res);
+        return res;
+    }, src1);
+    test.deepEqual(data, ["a","abc","ade","af","af0g","af1h"]);
+    test.equal(res, "");
+
+    var names = {
+        a: "A",
+        b: {x: "Bx", y: "By"},
+        c: "C"
+    };
+    res = api.visit(function(ctx, k, v) {
+        if (k === null)
+            return {};
+        if (typeof v === "object") {
+            var res = {};
+            ctx[k] = res;
+            return res;
+        } else {
+            ctx[k] = v;
+            return null;
+        }
+    }, names);
+    test.deepEqual(res, names, util.dump("ACTUAL", res));
+});
+
 
 fixture.execute();
